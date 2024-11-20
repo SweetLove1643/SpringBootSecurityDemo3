76eab346c065edb92bf821a581df5d5ac02f433f
SweetLove1643 <phanuan028@gmail.com>
2024-11-20 15:51:11 +0700

    first commit

---
+++ src/main/java/vn/iostar/SpringbootSecurityDemo3Application.java
@@ -0,0 +1,13 @@
+package vn.iostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityDemo3Application.class, args);
+	}
+
+}
